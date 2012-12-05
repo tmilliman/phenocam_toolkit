060ae4c8385450674ce816cf226505c3986ebfea
Tom Milliman <thomas.milliman@unh.edu>
2012-12-05 14:40:44 -0500

    move calculation to toolbox

--- matlab/test_gcc_calculation.m
+++ matlab/test_gcc_calculation.m
@@ -2,16 +2,20 @@
 % test_gcc_calculation.m
 % 
 % matlab script to perform simple calculation of ROI means for RGB
-% brightness as well as calculate GCC for testing and evalution with other
-% programs
+% brightness as well as calculate GCC for testing and evalution.
 % 
+
+addpath('phenocam_toolkit');
+rehash;
 
 current_dt=datestr(now,0);
 disp(current_dt);
 
 imgfile='./test_data/harvard_2008_08_07_103137.jpg';
 maskfile='./test_data/harvard_deciduous_0001_01.tif';
+gcc_expected=0.4367;
 
+disp('=========================================');
 disp('test_gcc_calculation.m')
 disp(sprintf('image file: %s',imgfile));
 disp(sprintf('mask file: %s',maskfile));
@@ -23,31 +27,22 @@ mask = imread(maskfile,'tif');
 % read in image, print out error message if file is 
 img = imread(imgfile);
 
-% make sure they are the same dimensions
-if size(img,1) ~= size(mask,1) || size(img,2) ~= size(mask,2)  
-  error('Mask and input image are not the same dimensions.')
-end
-
-% split image into its components
-red = img(:,:,1);
-green = img(:,:,2);
-blue = img(:,:,3);
-    
-% calculate green chromatic coordinates    
-% load individual band values
-meanred = mean(mean(red(mask == 0)));
-meangreen = mean(mean(green(mask == 0)));
-meanblue = mean(mean(blue(mask == 0)));
+[meanred, meangreen, meanblue] = get_dn_means(img,mask);
 
 % calculate green chromatic coordinates
-gcc = meangreen / (meanred + meangreen + meanblue);
+gcc_roi = meangreen / (meanred + meangreen + meanblue);
 expected_gcc = 0.4367;
 disp('DN-R, DN-G, DN-B');
 dnstr = sprintf('%7.4f, %7.4f, %7.4f',meanred, meangreen, ...
 		meanblue);
 disp(dnstr);
-disp(sprintf('Calculated value: %7.4f',gcc));
+disp(sprintf('Calculated value: %7.4f',gcc_roi));
 disp(sprintf('Expected value: %7.4f',expected_gcc));
 
-    
+if (gcc_roi - gcc_expected) > 0.00005
+  disp('gcc calculation failed!');
+else
+  disp('gcc calculation succeeded!');
+end
+
 return;
